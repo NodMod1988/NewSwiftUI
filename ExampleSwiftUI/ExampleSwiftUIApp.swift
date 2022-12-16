4b497c59efc1cbb7bc9601322c8a61a0d6ccb738
NodMod1988 <niewerth.ruben@gmail.com>
2022-12-16 13:38:56 +0100

    Initial Commit

---
+++ ExampleSwiftUI/ExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIApp.swift
+//  ExampleSwiftUI
+//
+//  Created by Ruben Niewerth on 16.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
